a5f322abb995c81cbc91cc17be1c3d6b39228656
imgarrrp <193384261+imgarrrp@users.noreply.github.com>
2025-02-23 17:11:18 +0900

    Add .config/powershell/profile.ps1

---
+++ dot_config/powershell/profile.ps1
@@ -0,0 +1,22 @@
+# ================================================================================
+# - PSReadLine
+# ================================================================================
+
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord Ctrl+v -Function Paste
+Set-PSReadLineKeyHandler -Chord Ctrl+i -Function Complete
+
+# ================================================================================
+# - PSStyle
+# ================================================================================
+
+if (Get-Variable -Name PSStyle -ErrorAction Ignore) {
+	$PSStyle.FileInfo.Directory = $PSStyle.Reset
+}
+
+# ================================================================================
+# - Variables, Aliases, Functions
+# ================================================================================
+
+$config = "$HOME\.config"
