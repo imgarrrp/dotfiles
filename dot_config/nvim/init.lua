1b99048ae9e322a542b2b63ed1481743955c1f02
imgarrrp <193384261+imgarrrp@users.noreply.github.com>
2025-02-23 17:02:38 +0900

    Add .config/nvim/init.lua

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,8 @@
+vim.opt.backup = false
+vim.opt.cursorline = true
+vim.opt.number = true
+vim.opt.ruler = false
+vim.opt.showcmd = false
+vim.opt.swapfile = false
+vim.opt.title = true
+vim.opt.wrap = false
